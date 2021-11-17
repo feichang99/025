24ff0a8b6cdd23e728536735bed15be1e7ad4702
feichang99 <30334910+feichang99@users.noreply.github.com>
2021-11-17 18:49:07 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '111',
+  ver: '112',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -15,8 +15,8 @@ jsproxy_config({
     'demo-hk': {
       label: 'feichang99-calm-sina点',
       lines: {
-        'calm-snowflake-504d.029sina.workers.dev': 1,
-        'crimson-snow-3307.029sina.workers.dev': 2,
+        'production.a.029hh.workers.dev': 1,
+        '029.jh2.029hh.workers.dev': 2,
       }
    },
     'demo-sg': {
